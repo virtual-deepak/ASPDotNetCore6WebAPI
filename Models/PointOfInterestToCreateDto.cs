7e2d30bae1c720bce3460ab10b239a0db38d4016
Deepak Agarwal <emailvirtualdeepak@gmail.com>
2022-06-19 15:44:49 +0530

    Validation of Input Model with Annotations

--- Models/PointOfInterestToCreateDto.cs
+++ Models/PointOfInterestToCreateDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DotNetCoreWebAPI.Models
 {
     public class PointOfInterestToCreateDto
     {
+        [Required(ErrorMessage = "Name field is required")]
+        [MaxLength(20)]
         public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
