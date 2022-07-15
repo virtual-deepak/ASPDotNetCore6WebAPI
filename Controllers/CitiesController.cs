0403d89b4a692f1fbc41e58c7f63d39029ef0d45
Deepak Agarwal <emailvirtualdeepak@gmail.com>
2022-07-15 08:43:15 +0530

    Controllers with multiple versions combinations

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace DotNetCoreWebAPI.Controllers
 {
     [ApiController]
-    [Route("api/cities")]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/cities")]
     public class CitiesController : ControllerBase
     {
         private readonly ICityRepository cityRepository;
